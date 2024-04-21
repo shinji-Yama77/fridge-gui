119fa9414b53bdf8afe436717ef1b7b747862896
Shinji Yamashita <shinjiyamashita@shins-M3.local>
2024-04-20 21:57:10 -0700

    base app

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import customtkinter
+
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+
+app = customtkinter.CTk()
+app.geometry("800x600")
+
+
+
+
+app.mainloop()
